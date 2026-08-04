aade2c8fd04af9bc3e379b93ff1717a2a05d39d3
Ritam771 <ritammajumdar8@gmail.com>
2026-08-04 19:45:25 +0530

    Implement anagram checking function in Python

---
+++ HashMap/Valid anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = {}
+        for ch in s:
+            if ch in count:
+                count[ch] += 1
+            else:
+                count[ch] = 1
+        for ch in t:
+            if ch not in count or count[ch] == 0:
+                return False
+            else:
+                count[ch] -= 1 
+        for value in count.values():
+            if value != 0 :
+                return False
+            
+        return True    
+                       
