b8d1a3b24a3c62e811e54e84702a1f84261395f1
Ritam771 <ritammajumdar8@gmail.com>
2026-08-16 10:33:18 +0530

    Update 3 Sum.py

--- Two-Pointers/3 Sum.py
+++ Two-Pointers/3 Sum.py
@@ -2,23 +2,23 @@ class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         res = []
         nums.sort()
-        for i in range (len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-
-            l = i + 1
-            r = len(nums) - 1 
-            while l < r:
-                s = nums[i] + nums[l] + nums[r]
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
                 if s > 0:
-                    r -= 1
+                  right  -= 1
                 elif s < 0:
-                    l += 1
-                else:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l - 1] :
-                        l += 1
-                    while l < r and nums[r] == nums[r - 1]:
-                        r -= 1
-        return res              
+                  left += 1
+                else: 
+                  res.append((nums[i],nums[left],nums[right]))
+                  while left < right and nums[left] == nums[left + 1]:
+                     left += 1
+                  while left < right and nums[right] == nums[right - 1]:
+                     right -= 1
+                  left += 1
+                  right -= 1
+        return res                     
